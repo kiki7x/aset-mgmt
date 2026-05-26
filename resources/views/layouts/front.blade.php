f19f6c88315b081b13aa19301c62f673007d2683
Yoga Apriadi <myogaapriadi43@gmail.com>
2026-05-26 17:57:03 +0800

    memperbaiki reload

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -17,16 +17,24 @@
     {{-- <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet"> --}}
     {{-- pakai font poppins --}}
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <!-- Vendor CSS Files -->
-    <link href="{{ asset('arsha/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('arsha/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <!-- Vendor CSS Files (preload critical styles to avoid FOUC) -->
+    <link rel="preload" as="style" href="{{ asset('arsha/assets/vendor/bootstrap/css/bootstrap.min.css') }}" onload="this.rel='stylesheet'">
+    <link rel="preload" as="style" href="{{ asset('arsha/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" onload="this.rel='stylesheet'">
     {{-- <link href="{{ asset('arsha/assets/vendor/aos/aos.css') }}" rel="stylesheet"> --}}
-    <link href="{{ asset('arsha/assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('arsha/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
-    <!-- Template Main CSS File -->
-    <link href="{{ asset('arsha/assets/css/main.css') }}" rel="stylesheet">
+    <link rel="preload" as="style" href="{{ asset('arsha/assets/vendor/glightbox/css/glightbox.min.css') }}" onload="this.rel='stylesheet'">
+    <link rel="preload" as="style" href="{{ asset('arsha/assets/vendor/swiper/swiper-bundle.min.css') }}" onload="this.rel='stylesheet'">
+    <!-- Template Main CSS File (preload + onload swap) -->
+    <link rel="preload" as="style" href="{{ asset('arsha/assets/css/main.css') }}" onload="this.rel='stylesheet'">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome-free/css/all.min.css') }}">
+    <link rel="preload" as="style" href="{{ asset('assets/plugins/fontawesome-free/css/all.min.css') }}" onload="this.rel='stylesheet'">
+    <noscript>
+        <link href="{{ asset('arsha/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('arsha/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+        <link href="{{ asset('arsha/assets/vendor/glightbox/css/glightbox.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('arsha/assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('arsha/assets/css/main.css') }}" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome-free/css/all.min.css') }}">
+    </noscript>
 
     <!-- =======================================================
   * Template Name: Arsha
@@ -39,9 +47,41 @@
   {{-- script tambahan --}}
   @stack('script-head')
   {{-- ./script tambahan --}}
+    <!-- Prevent flash of unstyled content (FOUC) while CSS loads -->
+    <style>
+        /* Hide page until fully loaded to avoid FOUC */
+        body.front-loading { visibility: hidden; }
+        /* Simple preloader style (minimal) */
+        #preloader {
+            position: fixed;
+            left: 0;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            background: #fff;
+            z-index: 99999;
+        }
+        #preloader .spinner {
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+            font-size: 14px;
+            color: #444;
+        }
+    </style>
+
+    <noscript>
+        <style>
+            /* If JS is disabled, show the page and hide preloader */
+            body.front-loading { visibility: visible; }
+            #preloader { display: none; }
+        </style>
+    </noscript>
+
 </head>
 
-<body class="index-page">
+<body class="front-loading index-page">
     <x-frontsite.header></x-frontsite.header>
         @yield('content')
     <x-frontsite.footer></x-frontsite.footer>
@@ -77,6 +117,26 @@
     {{-- script tambahan --}}
     @stack('script-foot')
     {{-- ./script tambahan --}}
+
+    <script>
+        // Remove preloader and reveal page when all resources are loaded.
+        (function(){
+            function finish() {
+                try {
+                    document.body.classList.remove('front-loading');
+                    var p = document.getElementById('preloader');
+                    if (p) { p.style.display = 'none'; }
+                } catch(e) { /* ignore */ }
+            }
+            if (document.readyState === 'complete') {
+                finish();
+            } else {
+                window.addEventListener('load', finish);
+                // Fallback: reveal after 2.5s in case load doesn't fire timely
+                setTimeout(finish, 2500);
+            }
+        })();
+    </script>
 </body>
 
 </html>
