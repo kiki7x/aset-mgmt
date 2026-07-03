75283a67f146f02abd22fb4de25aa369f5be1796
kiki7x <kiki7x@gmail.com>
2026-07-03 14:52:18 +0800

    Update files.blade.php

--- resources/views/admin/detailaset/files.blade.php
+++ resources/views/admin/detailaset/files.blade.php
@@ -4,69 +4,63 @@
 ])
 
 @section('content-tab')
-    <div class="card card">
-        <div class="card-header">
-            <h3 class="card-title">Files</h3>
-            <div class="card-tools">
-                <button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#modalUploadFile">
-                    <i class="fas fa-upload"></i>
-                </button>
-            </div>
-        </div>
-        <div class="card-body">
-            <ul class="todo-list" id="fileslist" style="display:flex;flex-wrap:wrap;padding:0;list-style:none;">
-                @forelse ($asset->files as $file)
-                    @php
-                        $ext = strtolower(pathinfo($file->name, PATHINFO_EXTENSION));
-                        switch ($ext) {
-                            case 'pdf':
-                                $icon = 'far fa-file-pdf text-danger';
-                                break;
-                            case 'jpg':
-                            case 'jpeg':
-                            case 'png':
-                                $icon = 'far fa-file-image text-primary';
-                                break;
-                            default:
-                                $icon = 'far fa-file text-muted';
-                                break;
-                        }
-                        $filePath = storage_path('app/public/' . $file->file);
-                        $size = file_exists($filePath) ? filesize($filePath) : 0;
-                        if ($size >= 1048576) {
-                            $formatted = number_format($size / 1048576, 2) . ' MB';
-                        } elseif ($size >= 1024) {
-                            $formatted = number_format($size / 1024, 2) . ' KB';
-                        } else {
-                            $formatted = $size . ' B';
-                        }
-                    @endphp
-                    <li class="border bg-white shadow-sm rounded m-2 p-2" style="width:260px;height:70px;">
-                        <div class="row align-items-center flex-nowrap h-100">
-                            <div class="col-2 text-center px-0">
-                                <i class="{{ $icon }} fa-2x"></i>
-                            </div>
-                            <div class="col-10 pl-2 overflow-hidden">
-                                <div class="text-truncate" style="font-weight:500;font-size:14px;">
-                                    {{ $file->name }}
-                                </div>
-                                <small class="text-muted">
-                                    {{ $formatted }} ● {{ $file->created_at ? \Carbon\Carbon::parse($file->created_at)->format('d M Y') : '-' }}
-                                </small>
-                                <div class="float-right mt-1">
-                                    <a href="{{ asset('storage/' . $file->file) }}" target="_blank" class="text-dark mr-1"><i class="fa fa-eye"></i></a>
-                                    <a href="{{ asset('storage/' . $file->file) }}" download class="text-dark mr-1"><i class="fa fa-download"></i></a>
-                                    <a href="#" class="text-red btn-delete-file" data-file-id="{{ $file->id }}" data-file-name="{{ $file->name }}"><i class="fa fa-trash"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </li>
-                @empty
-                    <li class="w-100 text-center py-4 text-muted" style="list-style:none;">Belum ada file.</li>
-                @endforelse
-            </ul>
-        </div>
+    <div class="d-flex justify-content-end mb-3">
+        <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#modalUploadFile">
+            <i class="fas fa-upload"></i> Upload File
+        </button>
     </div>
+
+    <ul class="todo-list" id="fileslist" style="display:flex;flex-wrap:wrap;padding:0;list-style:none;">
+        @forelse ($asset->files as $file)
+            @php
+                $ext = strtolower(pathinfo($file->name, PATHINFO_EXTENSION));
+                switch ($ext) {
+                    case 'pdf':
+                        $icon = 'far fa-file-pdf text-danger';
+                        break;
+                    case 'jpg':
+                    case 'jpeg':
+                    case 'png':
+                        $icon = 'far fa-file-image text-primary';
+                        break;
+                    default:
+                        $icon = 'far fa-file text-muted';
+                        break;
+                }
+                $filePath = storage_path('app/public/' . $file->file);
+                $size = file_exists($filePath) ? filesize($filePath) : 0;
+                if ($size >= 1048576) {
+                    $formatted = number_format($size / 1048576, 2) . ' MB';
+                } elseif ($size >= 1024) {
+                    $formatted = number_format($size / 1024, 2) . ' KB';
+                } else {
+                    $formatted = $size . ' B';
+                }
+            @endphp
+            <li class="border bg-white shadow-sm rounded m-2 p-2" style="width:260px;height:70px;">
+                <div class="row align-items-center flex-nowrap h-100">
+                    <div class="col-2 text-center px-0">
+                        <i class="{{ $icon }} fa-2x"></i>
+                    </div>
+                    <div class="col-10 pl-2 overflow-hidden">
+                        <div class="text-truncate" style="font-weight:500;font-size:14px;">
+                            {{ $file->name }}
+                        </div>
+                        <small class="text-muted">
+                            {{ $formatted }} ● {{ $file->created_at ? \Carbon\Carbon::parse($file->created_at)->format('d M Y') : '-' }}
+                        </small>
+                        <div class="float-right mt-1">
+                            <a href="{{ asset('storage/' . $file->file) }}" target="_blank" class="text-dark mr-1"><i class="fa fa-eye"></i></a>
+                            <a href="{{ asset('storage/' . $file->file) }}" download class="text-dark mr-1"><i class="fa fa-download"></i></a>
+                            <a href="#" class="text-red btn-delete-file" data-file-id="{{ $file->id }}" data-file-name="{{ $file->name }}"><i class="fa fa-trash"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        @empty
+            <li class="w-100 text-center py-4 text-muted" style="list-style:none;">Belum ada file.</li>
+        @endforelse
+    </ul>
 
     <div class="modal fade" id="modalUploadFile" tabindex="-1">
         <div class="modal-dialog">
